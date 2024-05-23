2ecc0ef067a199d7b4ebe7be66f142fe8275d72b
IncredibleZuess <carlo.barnardo@gmail.com>
2024-05-23 11:44:54 +0200

    Implemented UsageStatsManager and did some cleanup

--- Vip_Project_1/app/src/main/java/com/example/vip_project_1/MainActivity.kt
+++ Vip_Project_1/app/src/main/java/com/example/vip_project_1/MainActivity.kt
@@ -1,36 +1,54 @@
 package com.example.vip_project_1
 
+import android.app.usage.UsageStatsManager
+import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-
 
 
 class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.Q)
+    private fun hasUsageStatsPermission(): Boolean {
+        val appOps = getSystemService(Context.APP_OPS_SERVICE) as android.app.AppOpsManager
+        val mode = appOps.unsafeCheckOpNoThrow("android:get_usage_stats", android.os.Process.myUid(),packageName)
+        return mode == android.app.AppOpsManager.MODE_ALLOWED
+    }
+    private fun trackUsage() {
+        val usageStatsManager = getSystemService(Context.USAGE_STATS_SERVICE) as UsageStatsManager
+        val currentTime = System.currentTimeMillis()
+        val stats = usageStatsManager.queryUsageStats(UsageStatsManager.INTERVAL_DAILY, currentTime- 1000*60*60*24, currentTime)
+        if (stats != null) {
+            for (usageStats in stats) {
+                Log.d("UsageStats", "Package: ${usageStats.packageName}, Total Time: ${usageStats.totalTimeInForeground}")
+            }
+        }
+    }
+    @RequiresApi(Build.VERSION_CODES.Q)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-
-
-            val Registerbtn = findViewById<Button>(R.id.signup_button)
-        Registerbtn.setOnClickListener{
-            val Intent = Intent(this , MainActivity2::class.java)
-            startActivity(Intent)}
-
-
-            val Loginbtn = findViewById<Button>(R.id.login_button)
-            Loginbtn.setOnClickListener {
-                val Intent = Intent(this , home_Activity::class.java)
-                startActivity(Intent)
+        if (!hasUsageStatsPermission()) {
+            startActivity(Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS))
+        } else {
+            trackUsage()
+        }
+        val registerButton = findViewById<Button>(R.id.signup_button)
+        registerButton.setOnClickListener{
+        val intent = Intent(this , MainActivity2::class.java)
+        startActivity(intent)
+        }
+        val loginBtn = findViewById<Button>(R.id.login_button)
+        loginBtn.setOnClickListener {
+            val intent = Intent(this , home_Activity::class.java)
+            startActivity(intent)
             }
-
-
-
-
         }
     }
