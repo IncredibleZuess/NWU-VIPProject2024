021240fc93d1022f25335b562ffc3c6337cca554
IncredibleZuess <carlo.barnardo@gmail.com>
2024-06-06 21:40:11 +0200

    Weird git shit

--- Vip_Project_1/app/src/main/java/com/example/vip_project_1/HomeFragment.kt
+++ Vip_Project_1/app/src/main/java/com/example/vip_project_1/HomeFragment.kt
@@ -18,56 +18,98 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
-class HomeFragment : Fragment() {
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
 
+class HomeFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
     private lateinit var recyclerView: RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        // Unregister the broadcast receiver when the view is destroyed
+        activity?.unregisterReceiver(dataChangeReceiver)
     }
 
 
     // Initiate the usage stats manager and query the usage stats for the last 24 hours and filter by user installed apps
     private fun displayUsageStats() {
 
-        val usageStatsManager = activity?.getSystemService(Context.USAGE_STATS_SERVICE) as UsageStatsManager
+        val usageStatsManager =
+            activity?.getSystemService(Context.USAGE_STATS_SERVICE) as UsageStatsManager
         val currentTime = System.currentTimeMillis()
         // Query the usage stats for the last 24 hours and filter by user installed apps
-        val stats = usageStatsManager.queryUsageStats(UsageStatsManager.INTERVAL_DAILY, currentTime- 1000*60*60*24, currentTime)
-        val userInstalledApps = activity?.packageManager?.queryIntentActivities(Intent(Intent.ACTION_MAIN, null).addCategory(Intent.CATEGORY_LAUNCHER), 0)
+        val stats = usageStatsManager.queryUsageStats(
+            UsageStatsManager.INTERVAL_DAILY,
+            currentTime - 1000 * 60 * 60 * 24,
+            currentTime
+        )
+        val userInstalledApps = activity?.packageManager?.queryIntentActivities(
+            Intent(
+                Intent.ACTION_MAIN,
+                null
+            ).addCategory(Intent.CATEGORY_LAUNCHER), 0
+        )
         // Sort the usage stats by total time in foreground and setup the recycler view
         if (stats != null) {
             val sortedStats = stats.sortedByDescending { it.totalTimeInForeground }
-            val filteredStats = sortedStats.filter { userInstalledApps?.any { app -> app.activityInfo.packageName == it.packageName } == true }
+            val filteredStats =
+                sortedStats.filter { userInstalledApps?.any { app -> app.activityInfo.packageName == it.packageName } == true }
             setupRecyclerView(filteredStats)
         }
     }
+
     // Setup the recycler view with the usage stats
     private fun setupRecyclerView(usageStats: List<UsageStats>) {
         recyclerView.layoutManager = LinearLayoutManager(activity)
         recyclerView.adapter = UsageStatsAdapter(usageStats)
     }
+
     // Check if the user has granted the usage stats permission
     @RequiresApi(Build.VERSION_CODES.Q)
     private fun hasUsageStatsPermission(): Boolean {
-        val appOps = activity?.getSystemService(Context.APP_OPS_SERVICE) as android.app.AppOpsManager
+        val appOps =
+            activity?.getSystemService(Context.APP_OPS_SERVICE) as android.app.AppOpsManager
         val mode = activity?.packageName?.let {
-            appOps.unsafeCheckOpNoThrow("android:get_usage_stats", android.os.Process.myUid(),
+            appOps.unsafeCheckOpNoThrow(
+                "android:get_usage_stats", android.os.Process.myUid(),
                 it
             )
         }
         return mode == android.app.AppOpsManager.MODE_ALLOWED
     }
 
+    // BroadcastReceiver to handle data change events
+    private val dataChangeReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            if (intent?.action == "com.example.vip_project_1.SHOW_DATA_CHANGED") {
+                val sharedPreferencesData =
+                    activity?.getSharedPreferences("ShowData", Context.MODE_PRIVATE)
+                val showData = sharedPreferencesData?.getBoolean("ShowData", false) ?: false
+                if (showData) {
+                    displayUsageStats()
+                } else {
+                    recyclerView.adapter = null
+                }
+            }
+        }
+    }
 
 
-    @RequiresApi(Build.VERSION_CODES.Q)
+
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -82,62 +124,24 @@ class HomeFragment : Fragment() {
         } else {
             displayUsageStats()
 
-        recyclerView = view.findViewById<RecyclerView>(R.id.recyclerView)
+            recyclerView = view.findViewById<RecyclerView>(R.id.recyclerView)
 
-        val sharedPreferencesData = activity?.getSharedPreferences("ShowData", Context.MODE_PRIVATE)
-        val showData = sharedPreferencesData?.getBoolean("ShowData", false)
-        if (showData == true) {
-            if (!hasUsageStatsPermission()) {
-                startActivity(Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS))
-            } else {
-                displayUsageStats()
-            }
-        }
-        // Register broadcast receiver to listen for SHOW_DATA_CHANGED intent
-        val filter = IntentFilter("com.example.vip_project_1.SHOW_DATA_CHANGED")
-        activity?.registerReceiver(dataChangeReceiver, filter)
-
-
-
-        return view
-    }
-    // BroadcastReceiver to handle data change events
-    private val dataChangeReceiver = object : BroadcastReceiver() {
-        override fun onReceive(context: Context?, intent: Intent?) {
-            if (intent?.action == "com.example.vip_project_1.SHOW_DATA_CHANGED") {
-                val sharedPreferencesData = activity?.getSharedPreferences("ShowData", Context.MODE_PRIVATE)
-                val showData = sharedPreferencesData?.getBoolean("ShowData", false) ?: false
-                if (showData) {
-                    displayUsageStats()
+            val sharedPreferencesData =
+                activity?.getSharedPreferences("ShowData", Context.MODE_PRIVATE)
+            val showData = sharedPreferencesData?.getBoolean("ShowData", false)
+            if (showData == true) {
+                if (!hasUsageStatsPermission()) {
+                    startActivity(Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS))
                 } else {
-                    recyclerView.adapter = null
+                    displayUsageStats()
                 }
             }
-        }
-    }
-    override fun onDestroyView() {
-        super.onDestroyView()
-        // Unregister the broadcast receiver when the view is destroyed
-        activity?.unregisterReceiver(dataChangeReceiver)
-    }
+            // Register broadcast receiver to listen for SHOW_DATA_CHANGED intent
+            val filter = IntentFilter("com.example.vip_project_1.SHOW_DATA_CHANGED")
+            activity?.registerReceiver(dataChangeReceiver, filter, Context.RECEIVER_NOT_EXPORTED)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+
+        }
+        return view
     }
 }
